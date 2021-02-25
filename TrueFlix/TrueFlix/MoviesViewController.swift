59d7042d9d152be8ffc9168614eb4a37d47f47a2
TrayJames <65936907+TrayJames@users.noreply.github.com>
2021-02-24 21:07:57 -0500

    Unit2 Submission for TrueFlix

--- TrueFlix/TrueFlix/MoviesViewController.swift
+++ TrueFlix/TrueFlix/MoviesViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Alamofire
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
  
     
@@ -32,7 +32,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             
               self.movies = dataDictionary["results"] as! [[String:Any]]
               self.tableView.reloadData()
-              print(dataDictionary)
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -58,19 +57,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let baseUrl="https://image.tmdb.org/t/p/w185"
         let posterPath=movie ["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
+        cell.posterView.af_setImage(withURL:posterUrl!)
         
         
         
         return cell
     }
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("Loading up the details screen")
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController=segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
 }
